4864e4124d2ba6735daac489eccb1978851ca783
eaw1ch <eaw1ch@mail.ru>
2024-03-06 16:46:35 +0300

    1v

---
+++ js/script.js
@@ -0,0 +1,70 @@
+let swiper = new Swiper('.slide-content', {
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        375: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+    },
+})
+
+const iconMenu = document.querySelector('.header__toggle')
+const menuBody = document.querySelector('.header__navigation')
+
+if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active')
+        menuBody.classList.toggle('_active')
+    })
+}
+
+const menuPoints = document.querySelectorAll('.header__menu--point[data-goto]')
+
+if (menuPoints.length !== 0) {
+    menuPoints.forEach((menuPoint) => {
+        menuPoint.addEventListener('click', onMenuPointClick)
+    })
+}
+
+function onMenuPointClick(e) {
+    const menuPoint = e.target
+    if (
+        menuPoint.dataset.goto &&
+        document.querySelector(menuPoint.dataset.goto)
+    ) {
+        const gotoBlock = document.querySelector(menuPoint.dataset.goto)
+        const gotoBlockValue =
+            gotoBlock.getBoundingClientRect().top + pageYOffset
+
+        if (iconMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock')
+            iconMenu.classList.remove('_active')
+            menuBody.classList.remove('_active')
+        }
+
+        window.scrollTo({
+            top: gotoBlockValue,
+            behavior: 'smooth',
+        })
+        e.preventDefault()
+    }
+}
